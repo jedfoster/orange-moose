3680c2a77f342919d8b37818c6acc9918e31b845
Jed Foster <jed@jedfoster.com>
2021-01-18 11:36:42 -0800

    initial commit

--- src/ToDo.js
+++ src/ToDo.js
@@ -9,6 +9,31 @@ import React, { useState } from "react";
     for the structure of that prop.
 */
 
-const ToDo = () => <i>todos go here</i>;
+const ToDo = (props) => {
+  const [todoItems, setTodoItems] = useState(props.data);
 
+  return (
+    <div>
+      <input placeholder="Add Item"></input>
+      <button className="btn">Add</button>
+      <ul>
+        {todoItems.map((item, index) => (
+          <li className={item.completed ? "completed" : ""}>{item.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 export default ToDo;
+
+//list that render to the UI
+//traverse through the list using map
+//display the text to the UI
+
+// props.data.map((item, index) => {})
+
+//array1.map(x => x * 2)
+
+// <li class="completed">Apples</li>
+
+// https://codesandbox.io/s/sad-fire-kk3ln
