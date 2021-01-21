d4f5ecb0fe7a35596a4e265577ae30133f52d4df
Jed Foster <jed@jedfoster.com>
2021-01-20 16:03:32 -0800

    initial commit

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,5 +1,5 @@
 import "./ToDo.css";
-import React, { useState } from "react";
+import React from "react";
 
 /*
   Notes:
@@ -8,7 +8,71 @@ import React, { useState } from "react";
     data in a prop, `data`. See `src/data.json` 
     for the structure of that prop.
 */
+class ToDo extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      items: props.data,
+      newItemText: ""
+    };
+  }
 
-const ToDo = () => <i>todos go here</i>;
+  addNewItem(e) {
+    e.preventDefault();
+
+    const newItem = {
+      text: this.state.newItemText,
+      completed: false
+    };
+
+    this.setState({
+      items: this.state.items.concat(newItem),
+      newItemText: ""
+    });
+  }
+
+  handleCheckboxClick(i) {
+    const newState = this.state;
+    newState.items[i].completed = !newState.items[i].completed;
+
+    this.setState(newState);
+  }
+
+  handleFormSubmission(e) {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <form
+          onSubmit={(e) => this.addNewItem(e)}>
+          <input
+            placeholder="new item"
+            value={this.state.newItemText}
+            onChange={(e) =>
+              this.setState({ newItemText: e.currentTarget.value })
+            }
+          />
+          <button>add</button>
+        </form>
+        <ul className="todo-list">
+          {this.state.items.map((item, i) => {
+            return (
+              <li className={item.completed ? "completed-item" : ""}>
+                <input
+                  type="checkbox"
+                  checked={item.completed}
+                  onClick={() => this.handleCheckboxClick(i)}
+                />
+                {item.text}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default ToDo;
