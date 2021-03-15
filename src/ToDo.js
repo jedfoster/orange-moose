7811c1d384349a44ae353f1e24a1f6747cd3771e
Jed Foster <jed@jedfoster.com>
2021-03-15 12:38:42 -0700

    df

--- src/ToDo.js
+++ src/ToDo.js
@@ -9,6 +9,51 @@ import React, { useState } from "react";
     for the structure of that prop.
 */
 
-const ToDo = () => <i>todos go here</i>;
+const ToDo = (props) => {
+  const [newItem, setNewItem] = useState("");
+  const [list, addList] = useState(props.data);
+
+  const onChangeHandler = (event) => {
+    setNewItem(event.target.value);
+  };
+
+  const onClick = (event) => {
+    let newList = [...list];
+    newList.push({ text: newItem, completed: false });
+    addList(newList);
+    event.preventDefault();
+  };
+
+  return (
+    <form>
+      <input
+        className="prettyCorners"
+        id="newItem"
+        type="text"
+        placeholder="new item"
+        onChange={onChangeHandler}
+      ></input>
+      <button className="buttonClass" onClick={onClick}>
+        Add
+      </button>
+      {list.map((item) => {
+        return (
+          <div>
+            <label>
+              <input
+                type="radio"
+                checked={item.completed}
+                disabled={item.completed}
+              />
+              <span className={item.completed ? "itemCompleted" : ""}>
+                {item.text}
+              </span>
+            </label>
+          </div>
+        );
+      })}
+    </form>
+  );
+};
 
 export default ToDo;
