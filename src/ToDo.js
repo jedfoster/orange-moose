447b6ddf7a94fb6106f3a1febe405eb5ebccbd40
Jed Foster <jed@jedfoster.com>
2021-03-15 14:24:26 -0700

    initial commit

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,5 +1,5 @@
 import "./ToDo.css";
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 
 /*
   Notes:
@@ -9,6 +9,46 @@ import React, { useState } from "react";
     for the structure of that prop.
 */
 
-const ToDo = () => <i>todos go here</i>;
+const ToDo = (props) => {
+  const [itemName, setItemName] = useState("");
+  const [itemData, setItemData] = useState(props.data);
+
+  const handleAddClick = useCallback(() => {
+    if (itemName === "") return;
+    setItemData([
+      ...itemData,
+      {
+        completed: false,
+        text: itemName
+      }
+    ]);
+  }, [itemName]);
+
+  const handleItemNameChanged = useCallback((e) => {
+    setItemName(e.target.value);
+  }, []);
+
+  return (
+    <React.Fragment>
+      <input id="name-input" type="text" onChange={handleItemNameChanged} />
+      <input
+        id="add-button"
+        type="button"
+        value="Add"
+        onClick={handleAddClick}
+      />
+      <ul>
+        {itemData.map((item, index) => {
+          return (
+            <li className={`${item.completed ? "item-complete" : ""}`}>
+              <input type="checkbox" id={index} checked={item.completed} />
+              <label htmlFor={index}>{item.text}</label>
+            </li>
+          );
+        })}
+      </ul>
+    </React.Fragment>
+  );
+};
 
 export default ToDo;
