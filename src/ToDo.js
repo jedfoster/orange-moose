624abffc63f5c1fe4ccb12d2cc2be5fd8c0a67e1
Jed Foster <jed@jedfoster.com>
2021-03-19 15:06:06 -0700

    initial commit

--- src/ToDo.js
+++ src/ToDo.js
@@ -9,6 +9,48 @@ import React, { useState } from "react";
     for the structure of that prop.
 */
 
-const ToDo = () => <i>todos go here</i>;
+const ToDo = ({ data }) => {
+  const [list, setList] = useState(data);
+  const [newItem, setNewItem] = useState("");
+
+  // const isChecked = useMemo(() => (
+  //   <li className="checked"></li>
+  // ))
+
+  const clickEvent = () => {
+    const newList = [...list];
+    newList.push({ complete: false, text: newItem });
+    setList(newList);
+  };
+
+  const changeCompleted = (index) => {
+    // newList[index].completed...
+    // setCompted(!complted)
+  };
+
+  // console.log(list);
+  return (
+    <>
+      <div>
+        <input
+          className="input"
+          placeholder="new item"
+          onChange={(e) => setNewItem(e.target.value)}
+        ></input>
+        <button className="button" onClick={clickEvent}>
+          Add
+        </button>
+      </div>
+      <ul>
+        {list.map((item, index) => (
+          <li className={item.completed ? "completed" : "incompleted"}>
+            <button onClick={changeCompleted(index)} />
+            {item.text}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
 export default ToDo;
