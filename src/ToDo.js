e3e970cb84e67f3a4ba6be60d1afd74c0f24320e
Jed Foster <jed@jedfoster.com>
2021-03-29 15:04:43 -0700

    initial commit

--- src/ToDo.js
+++ src/ToDo.js
@@ -9,6 +9,60 @@ import React, { useState } from "react";
     for the structure of that prop.
 */
 
-const ToDo = () => <i>todos go here</i>;
+const ToDo = ({ data }) => {
+  const [todoItems, setTodoItems] = useState(data);
+  const [newItemText, setNewItemText] = useState("");
+
+  const completeItem = (index) => {
+    const items = [...todoItems];
+
+    items[index].completed = !items[index].completed;
+
+    setTodoItems(items);
+  };
+
+  const addNewItem = () => {
+    const text = newItemText;
+    const items = [...todoItems];
+
+    items.push({
+      completed: false,
+      text
+    });
+
+    setTodoItems(items);
+    setNewItemText("");
+  };
+
+  return (
+    <>
+      <div>
+        <input
+          type="text"
+          value={newItemText}
+          onChange={(e) => setNewItemText(e.target.value)}
+        />
+        <button class="add-button" onClick={() => addNewItem()}>
+          Add
+        </button>
+      </div>
+      <ul>
+        {todoItems.map((item, i) => (
+          <li>
+            <input
+              type="checkbox"
+              checked={item.completed}
+              onChange={() => completeItem(i)}
+              className="todo-checkbox"
+            />
+            <span className={item.completed && "line-through-text"}>
+              {item.text}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
 export default ToDo;
