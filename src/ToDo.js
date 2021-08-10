1166e0d54fea9d08fb6df5564471830eb123863d
Jed Foster <jed@jedfoster.com>
2021-08-10 11:48:56 -0700

    Add IDs to items, import uuid package

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,5 +1,6 @@
 import "./ToDo.css";
 import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
 
 /*
   Notes:
@@ -7,6 +8,7 @@ import React, { useState } from "react";
   - Your `ToDo` component receives initial
     data in a prop, `data`. See `src/data.json` 
     for the structure of that prop.
+  - Call `uuid()` to generate IDs for your items.
 */
 
 const ToDo = () => <i>todos go here</i>;
