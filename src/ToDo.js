7e0e77c4b1439e1d9dac8ff7645f4e973b264fb0
Jed Foster <jed@jedfoster.com>
2021-08-24 14:30:28 -0700

    initial commit

--- src/ToDo.js
+++ src/ToDo.js
@@ -11,6 +11,55 @@ import { v4 as uuid } from "uuid";
   - Call `uuid()` to generate IDs for your items.
 */
 
-const ToDo = () => <i>todos go here</i>;
+const ToDo = (props) => {
+  const [list, setList] = useState(props.data);
+
+  const [newItem, setNewItem] = useState("");
+
+  var InputField = function (prop) {
+    return (
+      <input
+        id="add-item"
+        type="text"
+        onChange={(event) => {
+          setNewItem(event.target.value);
+          console.log(newItem);
+        }}
+        value={newItem}
+      />
+    );
+  };
+
+  var AddButton = function () {
+    return (
+      <button
+        type="submit"
+        onClick={() => {
+          setList([...list, { text: newItem, completed: false }]);
+        }}
+      >
+        Add
+      </button>
+    );
+  };
+
+  var ItemList = function ({ list }) {
+    return (
+      <ul>
+        {list.map((a) => (
+          <li>{a.text}</li>
+        ))}
+      </ul>
+    );
+  };
+
+  return (
+    <div>
+      <InputField />
+      <AddButton />
+      <ItemList list={list} />
+    </div>
+  );
+};
 
 export default ToDo;
