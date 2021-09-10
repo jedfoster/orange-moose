96699e5860df09ab9387e8c07ffe0b6478ed0060
Jed Foster <jed@jedfoster.com>
2021-09-10 15:15:20 -0700

    initial commit

--- src/ToDo.js
+++ src/ToDo.js
@@ -11,6 +11,46 @@ import { v4 as uuid } from "uuid";
   - Call `uuid()` to generate IDs for your items.
 */
 
-const ToDo = () => <i>todos go here</i>;
+const ToDo = ({ data }) => {
+  const [todoList, setTodoList] = useState(data);
+
+  function AddItem() {
+    var newItem = {};
+    newItem.id = uuid();
+    newItem.completed = false;
+    newItem.text = document.getElementById("newItem").value;
+    setTodoList([...todoList, newItem]);
+  }
+
+  function ItemDoneToggle(item) {
+    var newList = todoList.map((innerItem) => {
+      if (innerItem.id === item.id) innerItem.completed = !innerItem.completed;
+      return innerItem;
+    });
+    setTodoList(newList);
+  }
+
+  return (
+    <section>
+      <input id="newItem" />
+      <button id="addNew" onClick={AddItem}>
+        Add
+      </button>
+      <ul>
+        {todoList.map((item) => (
+          <li>
+            <input
+              id={item.id}
+              type="checkbox"
+              checked={item.completed}
+              onClick={(click) => ItemDoneToggle(item)}
+            />
+            <label id="lblCheckbox">{item.text}</label>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
 
 export default ToDo;
