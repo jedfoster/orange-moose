e7ccca9df0dd18a2df169eac62bf88139e13cfd0
Jed Foster <jed@jedfoster.com>
2021-09-14 16:45:59 -0700

    initial commit

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,6 +1,13 @@
 import "./ToDo.css";
 import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
+// import data from "./data.json";
+
+// 1. import existing data done
+// 2. loop done
+// 3. build template/ styles
+// 4. build the input to add done
+// 5. some functioning to toggle done
 
 /*
   Notes:
@@ -10,7 +17,64 @@ import { v4 as uuid } from "uuid";
     for the structure of that prop.
   - Call `uuid()` to generate IDs for your items.
 */
+const ToDo = (props) => {
+  const { data } = props;
+  const [newTodo, setNewTodo] = useState("");
+  const [list, setList] = useState(data);
+  function toggleChecked(todo) {
+    // todo.completed = !todo.completed;
+    // iterate over the list
+    // find thar uuid
+    // change it
+    // make new list
+    // set newlist
 
-const ToDo = () => <i>todos go here</i>;
+    const thing = list.map((item) => {
+      if (item.id === todo.id) {
+        item.completed = !item.completed;
+      }
+
+      return item;
+    });
+
+    setList(thing);
+  }
+  function todoChangeHandler(e) {
+    setNewTodo(e.target.value);
+  }
+
+  function handleAdd() {
+    let newList = [
+      ...list,
+      {
+        id: uuid(),
+        completed: false,
+        text: newTodo
+      }
+    ];
+
+    setList(newList);
+    setNewTodo("");
+  }
+
+  return (
+    <>
+      <input type="text" onChange={todoChangeHandler} value={newTodo} />
+      <button onClick={handleAdd}> Add</button>
+      <ul>
+        {list.map((todo) => (
+          <li key={todo.id}>
+            <input
+              type="checkbox"
+              onChange={() => toggleChecked(todo)}
+              checked={todo.completed}
+            />
+            {todo.text}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
 export default ToDo;
